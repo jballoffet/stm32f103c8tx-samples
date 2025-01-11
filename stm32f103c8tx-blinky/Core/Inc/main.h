cba340723f6b36d191d246cad36c6ed7b6c7cbe1
Javier Balloffet <javier.balloffet@gmail.com>
2025-01-11 19:28:48 +0100

    Update blinky example

---
+++ stm32f103c8tx-blinky/Core/Inc/main.h
@@ -0,0 +1,20 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f1xx_hal.h"
+
+/**
+ * @brief This function is executed in case of error occurrence.
+ * @return None.
+ */
+void Error_Handler(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __MAIN_H
---
+++ stm32f103c8tx-blinky/Core/Src/main.c
@@ -0,0 +1,103 @@
+#include "main.h"
+
+/**
+ * @brief System clock configuration.
+ * @return None.
+ */
+void SystemClock_Config(void);
+
+/**
+ * @brief GPIO initialization.
+ * @return None.
+ */
+static void MX_GPIO_Init(void);
+
+/**
+ * @brief Application entry point.
+ * @return Execution final status.
+ */
+int main(void) {
+  // MCU Configuration
+  // Reset of all peripherals, initializes the Flash interface and the Systick.
+  HAL_Init();
+
+  // Configure the system clock.
+  SystemClock_Config();
+
+  // Initialize all configured peripherals.
+  MX_GPIO_Init();
+
+  // Infinite loop.
+  while (1) {
+    // Blink STM32 Blue Pill's led at 1 Hz.
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    HAL_Delay(500);
+  }
+}
+
+void SystemClock_Config(void) {
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  // Initializes the RCC Oscillators according to the specified parameters
+  // in the RCC_OscInitTypeDef structure.
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+    Error_Handler();
+  }
+
+  // Initializes the CPU, AHB and APB buses clocks.
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+    Error_Handler();
+  }
+}
+
+static void MX_GPIO_Init(void) {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  // GPIO Ports Clock Enable.
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  // Configure GPIO pin Output Level.
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+
+  // Configure GPIO pin : PC13.
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+}
+
+void Error_Handler(void) {
+  __disable_irq();
+  while (1) {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file pointer to the source file name.
+ * @param  line assert_param error line source number.
+ * @return None.
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
+}
+#endif /* USE_FULL_ASSERT */
